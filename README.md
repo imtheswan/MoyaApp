1183d2c1fee32eefe85ccc9d96910428d299486f
imtheswan <juanpamora31@gmail.com>
2022-10-05 21:19:39 -0500

    Splash funciona en Android 7, pero no en 12

---
+++ README.md
@@ -0,0 +1,2 @@
+##Splash, Register, Login y CRUD con Archivos y JSON
+Moya Regalado Juan Pablo 5IM10
---
+++ app/src/main/java/com/example/moyaparcial2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.moyaparcial2;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Crear objto tipo action bar, la barrita encima de la aplicacion
+        ActionBar actionBar = getSupportActionBar();//Regresa el action bar de la actividad
+        actionBar.hide();// La oculta
+        Handler handler = new Handler(); //Crear nuevo handler
+        //Handler permite correr runnables y mandar mensajes asociados a Threads
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Home.class); //Literal
+                // un intent es para saltar a activities e incluso otras aplicaciones
+                startActivity(intent);
+                finish(); // Termina la actividad y no podremos volver a ella, podriamos hacerlo si no la terminaramos
+            }
+        }, 1000);
+        //finish(); // Termina la actividad y no podremos volver a ella, podriamos hacerlo si no la terminaramos
+    }
+}
