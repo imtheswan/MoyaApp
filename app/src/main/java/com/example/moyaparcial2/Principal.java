aef71033374245ab0088ea1a7abe73d49bf989bc
imtheswan <juanpamora31@gmail.com>
2022-10-14 04:50:30 -0500

    Se asdfghjkl

--- app/src/main/java/com/example/moyaparcial2/Principal.java
+++ app/src/main/java/com/example/moyaparcial2/Principal.java
@@ -1,5 +1,6 @@
 package com.example.moyaparcial2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
