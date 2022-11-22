c8e409eb032a305a6f08aa8d1b9ca51cfd187859
imtheswan <juanpamora31@gmail.com>
2022-11-22 07:46:02 -0600

    Password manager started

---
+++ MoyaApp2/app/src/main/java/com/example/moyaapp2/PasswordManager.java
@@ -0,0 +1,14 @@
+package com.example.moyaapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PasswordManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_manager);
+    }
+}
